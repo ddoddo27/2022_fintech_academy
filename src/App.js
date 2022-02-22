efb71dc2e7072d6c5432c1f8a8ec25ddd48eb92b
ddoddo27 <ddoddo_27@naver.com>
2022-02-22 23:43:25 +0900

    fintech_academy react 실습

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import ListComponent from "./components/ListComponent";
+import HeaderWhite from "./components/HeaderWhite";
+
+function App() {
+  return (
+    <div>
+      <HeaderWhite title="환경설정" />
+      <ListComponent></ListComponent>
+    </div>
+  );
+}
+
+export default App;

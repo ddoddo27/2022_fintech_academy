a1da07875328e0261a0d86f9a46116cfb64489a3
ddoddo27 <ddoddo_27@naver.com>
2022-02-23 13:45:32 +0900

    뉴스api검색

--- src/components/HeaderWhite.js
+++ src/components/HeaderWhite.js
@@ -2,15 +2,16 @@ import React from "react";
 import styled from "styled-components";
 
 const HeaderBlock = styled.div`
-  border-bottom: 1px solid #000;
+  border-bottom: 1px dotted #000;
   text-align: center;
   padding: 20px;
   font-size: 20px;
   font-weight: bold;
+  /* background-color: #1c6ea4; */
 `;
 
-const HeaderWhite = (props) => {
-  return <HeaderBlock>{props.title}</HeaderBlock>;
+const HeaderWhite = ({ title }) => {
+  return <HeaderBlock>{title}</HeaderBlock>;
 };
 
 export default HeaderWhite;
--- src/components/News/NewsList.js
+++ src/components/News/NewsList.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const NewsList = () => {
+const NewsList = ({searchResultList}) => {
   return <div>NewsList</div>;
 };
 
